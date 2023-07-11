4d559c7b4da78126320d033631459ea1f964998a
Vidar Rehn <vidar.rehn@gmail.com>
2023-07-11 10:46:25 +0200

    initial module stup

---
+++ lib/components/atoms/GeinsTikTok/GeinsTikTok.vue
@@ -0,0 +1,22 @@
+<template>
+    <div>
+        tiktok
+    </div>
+</template>
+<script>
+/* 
+  Tik tok
+*/
+
+export default {
+    name: 'GeinsTikTok',
+    mixins: [],
+    props: {},
+    data: () => ({}),
+    computed: {},
+    watch: {},
+    mounted() { },
+    methods: {}
+};
+</script>
+  
